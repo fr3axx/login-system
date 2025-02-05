fffc23d2388a443b0e103916b3caf0e27291e64e
Jerome Rojas <skreamlight@gmail.com>
2025-02-05 19:10:00 -0400

    deploy

--- core/settings.py
+++ core/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','moya-authentication.fly.dev']
+CSRF_TRUSTED_ORIGINS = ['https://moya-authentication.fly.dev']
 
 
 # Application definition
